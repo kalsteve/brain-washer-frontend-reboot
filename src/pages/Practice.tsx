582f563e1825f5982e4d2ce1ad726743fa297a37
김지민 <jimin1945@naver.com>
2024-07-09 21:54:49 +0900

    hello

---
+++ src/pages/Practice.tsx
@@ -0,0 +1,10 @@
+export default function Practice() {
+  const a = 1;
+  return (
+    <div className=" w-screen h-screen bg-red-600">
+      <div className="bg-black bottom-0 absolute">jimin</div>
+      <div className="bg-blue-400">good</div>
+      <div>dd</div>
+    </div>
+  );
+}
