36952e84e12b76b2bcbd2cb2febd5eded1bf33e6
김지민 <jimin1945@naver.com>
2024-07-12 15:55:01 +0900

    저장한 음성 및 이미지 퍼블리싱

--- src/pages/Practice.tsx
+++ src/pages/Practice.tsx
@@ -2,9 +2,10 @@ export default function Practice() {
   const a = 1;
   return (
     <div className=" w-screen h-screen bg-red-600">
-      <div className="bg-black bottom-0 absolute">jimin</div>
-      <div className="bg-blue-400">good</div>
-      <div>dd</div>
+      <div className="py-4 my-4 border border-gray-200 text-3xl shadow-2xl">
+        저장한 음성 및 이미지
+        <div></div>
+      </div>
     </div>
   );
 }
