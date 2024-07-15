c0f1fecfb2cc9cf52b52e792a379cc93deced1ec
dongmin <dongmin11566@gmail.com>
2024-07-15 15:32:23 +0900

    chore : env 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string; //이부분을 수정해준다. 위에 적은 변수의 타입을 적어준다
+  // 다른 환경 변수들에 대해 아래에 위에처럼 타입을 지정해준다
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
