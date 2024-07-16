fd06798ffcb81fbe72d990fb679192c2db210ae0
dongmin <dongmin11566@gmail.com>
2024-07-16 16:20:59 +0900

    feat : 채팅방 메뉴 조건부 렌더링 구현

--- src/api/voices.ts
+++ src/api/voices.ts
@@ -9,3 +9,27 @@ const getAllTts = async () => {
     console.error(error);
   }
 };
+
+// 특정 TTS 저장
+const postTts = async (bubble_id: string | undefined) => {
+  try {
+    const response = await axiosInstance.post(`/voices/${bubble_id}`, {});
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 채팅방별 TTS 목록 조회
+const getRoomTts = async (chat_id: number|null) => {
+  try {
+    const response = await axiosInstance.get(`/voices/chat/${chat_id}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getAllTts, postTts, getRoomTts };
