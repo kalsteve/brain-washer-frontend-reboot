948546422fee6e6c5c78a375118b128144ccc8c1
김지민 <jimin1945@naver.com>
2024-07-20 00:02:15 +0900

    docs : PlayAudioPage.tsx 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main";
 import Chat from "./pages/Chat";
-import ListBoard from "./pages/ListBoard.tsx";
+import ListBoard from "./pages/ListBoard";
+import PlayAudioPage from "./components/PlayAudioPage"; // Import your PlayAudioPage component here
 
 function App() {
   return (
@@ -10,16 +12,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/list_board" element={<ListBoard />} />
-        <Route
-          path="/chat/:chat_id"
-          element={
-            <Chat
-              name={"Andrew Park"}
-              description={"직설적인 피드백을 통한 냉철한 조언"}
-              image={"https://i.ibb.co/hFy5Cbz/2024-07-02-4-08-52.png"}
-            />
-          }
-        />
+        <Route path="/chat/:chat_id" element={<Chat
+          name={"Andrew Park"}
+          description={"직설적인 피드백을 통한 냉철한 조언"}
+          image={"https://i.ibb.co/hFy5Cbz/2024-07-02-4-08-52.png"}
+        />} />
+        <Route path="/play" element={<PlayAudioPage />} /> {/* Define the /play route */}
       </Routes>
     </BrowserRouter>
   );
